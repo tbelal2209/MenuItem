5c1ebbb3af765f76e3276aace55beecf4ced389e
BT_1N3_07@bktech.local <BT_1N3_07@bktech.local>
2018-11-27 08:41:28 -0500

    Initial commit

---
+++ src/com/company/Drink.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Drink implements MenuItem {
+private 
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+}
---
+++ src/com/company/MenuRunner.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class MenuRunner {
+
+    public static void main(String[] args) {
+	// write your code here
+        Sandwich reuben = new Sandwich(5.75,"Reuben");
+        Sandwich bec = new Sandwich(2.00,"BEC");
+        Salad waldorf = new Salad(7.2, "Waldorf");
+        Salad caesar = new Salad(4.50, "Caeser");
+        Drink icedTea = new Drink (1.50,"Iced Tea");
+        Drink smoothie = new Drink (5.50,"Smoothie");
+
+        Trio trio1 = new Trio(reuben,waldorf,icedTea);
+        Trio trio2 = new Trio(bec,caesar, smoothie);
+        Trio trio3 = new Trio(bec, caesar,icedTea);
+         System.out.println(trio1. getName() + "Price: " + trio1.getPrice());
+         System.out.println(trio2. getName() + "Price: " + trio2.getPrice());
+         System.out.println(trio3. getName() + "Price: " + trio3.getPrice());
+    }
+}
