bb22712779313ca37ae91b94ed27d1d26622cb6e
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-11-27 19:29:14 -0500

    Update Salad.java

--- src/com/company/Salad.java
+++ src/com/company/Salad.java
@@ -1,4 +1,19 @@
 package com.company;
 
-public class Salad {
+public class Salad implements MenuItem {
+    private double Price;
+    private String Name;
+    public Salad (double Price, String Name){
+        this.Name = Name;
+        this.Price = Price;
+    }
+    @Override
+    public String getName() {
+        return Name;
+    }
+
+    @Override
+    public double getPrice() {
+        return Price;
+    }
 }
