05e36f6f246d29a37096ee7b1452760af1e7d39a
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-11-27 19:29:46 -0500

    Update Sandwich.java

--- src/com/company/Sandwich.java
+++ src/com/company/Sandwich.java
@@ -1,4 +1,19 @@
 package com.company;
 
-public class Sandwich {
+public class Sandwich implements MenuItem {
+    private double Price;
+    private String Name;
+    public Sandwich (double Price, String Name){
+        this.Name = Name;
+        this.Price = Price;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+
+    public double getPrice() {
+        return Price;
+    }
 }
